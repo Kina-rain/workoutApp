260e315558eef2842861f9fe80e14c8499918b62
Kina Rain <tina@lawtk.net>
2020-02-22 21:58:20 -0500

    added dotenv to config

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const express = require("express")
-const logger = require("morgan")
-const mongoose = require("mongoose")
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+require('dotenv').config();
 
 // setting our port for Heroku and local
 const PORT = process.env.PORT || 8000;
