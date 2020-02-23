6c3803b37d628657038be0d9f936ced925074348
Kina Rain <tina@lawtk.net>
2020-02-22 22:07:28 -0500

    fixed mongodb uri

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json())
 app.use(express.static("public"))
 
 // connect to the database
-mongoose.connect(process.env.MONGDB_URI || "mongodb://localhost/workout", { useNewUrlParser: true })
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true })
 
 // importing my routes
 const routes = require("./routes/routes")
